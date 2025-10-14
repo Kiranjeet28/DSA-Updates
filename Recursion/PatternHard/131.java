f3d2046c248771a804b2ed14b45c98158e9e5fa9
kiranjeetkour144@gmail.com <kiranjeet kour>
2025-10-14 19:28:24 +0530

    Add Sudoku solver implementation with backtracking

--- Recursion/PatternHard/131.java
+++ Recursion/PatternHard/131.java
@@ -1,20 +1,22 @@
-public class Solution {
-    public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList<>();
-        backtrack(s, 0, new ArrayList<>(), result);
-        return result;
+import java.util.*;
+//rewrite for revision 
+class Solution {
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    private void backtrack(String s, int start, List<String> path, List<List<String>> result) {
-        // If we've reached the end of the string, add the current partition to the result list
-        if (start == s.length()) {
+    public void backtrack(String s, int start, List<String> path, List<List<String>> result) {
+        if (start == s.length() - 1) {
             result.add(new ArrayList<>(path));
-            return;
         }
-        // Explore all possible partitions
         for (int end = start + 1; end <= s.length(); end++) {
-            // If the current substring is a palindrome, add it to the current path
-            if (isPalindrome(s, start, end - 1)) {
+            //split string 
+            if (isPalindrome(s, start, end)) {
                 path.add(s.substring(start, end));
                 // Recur to find other partitions
                 backtrack(s, end, path, result);
@@ -24,13 +26,15 @@ public class Solution {
         }
     }
 
-    private boolean isPalindrome(String s, int left, int right) {
-        // Check if the substring s[left:right+1] is a palindrome
-        while (left < right) {
-            if (s.charAt(left++) != s.charAt(right--)) {
-                return false;
-            }
-        }
-        return true;
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), result);
+        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+       
+        Solution solution = new Solution();
+        solution.partition("racecar");
     }
 }
